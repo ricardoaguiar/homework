f26d1cf6f870ac60fd08f2cbdd5e514dcdfe619b
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-09-03 05:49:04 +0200

    function notes

--- javascript/javascript1/week2/notes.js
+++ javascript/javascript1/week2/notes.js
@@ -1,1 +1,14 @@
 
+//from preparation material
+
+let userName = 'John'; //variable outside the function (global)
+
+function showMessage() { //function declaration
+  //internal function variable (let message) accescible only inside the function
+  //the function concatenates the internal variable (message) with the global variable (userName) 
+  let message = 'Hello, ' + userName; //function body
+  alert(message); 
+}
+
+//calling the function > the function body code runs
+showMessage(); // Hello, John // result
