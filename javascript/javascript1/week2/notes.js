8729258dbedb69a9b108317c3087fb72ee0e850e
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-09-03 05:52:25 +0200

    my notes

--- javascript/javascript1/week2/notes.js
+++ javascript/javascript1/week2/notes.js
@@ -12,3 +12,36 @@ function showMessage() { //function declaration
 
 //calling the function > the function body code runs
 showMessage(); // Hello, John // result
+
+console.log("+++++++++++++++++++++++++++++");
+
+let userName = 'John';
+
+function showMessage() {
+  userName = "Bob"; // (1) changed the outer variable
+
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+alert( userName ); // John before the function call
+
+showMessage();
+
+alert( userName ); // Bob, the value was modified by the function
+
+console.log("+++++++++++++++++++++++++++++");
+
+let userName = 'John';
+
+function showMessage() {
+  let userName = "Bob"; // declare a local variable
+
+  let message = 'Hello, ' + userName; // Bob
+  alert(message);
+}
+
+// the function will create and use its own userName
+showMessage();
+
+alert( userName ); // John, unchanged, the function did not access the outer variable
