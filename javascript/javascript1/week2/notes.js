766a5687480f75b84116d37ec7c7ae0539824c74
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-09-04 06:54:44 +0200

    notes

--- javascript/javascript1/week2/notes.js
+++ javascript/javascript1/week2/notes.js
@@ -23,11 +23,8 @@ function showMessage() {
   let message = 'Hello, ' + userName;
   alert(message);
 }
-
 alert( userName ); // John before the function call
-
 showMessage();
-
 alert( userName ); // Bob, the value was modified by the function
 
 console.log("+++++++++++++++++++++++++++++");
@@ -43,5 +40,26 @@ function showMessage() {
 
 // the function will create and use its own userName
 showMessage();
-
 alert( userName ); // John, unchanged, the function did not access the outer variable
+
+console.log("+++++++++++++++++++++++++++++");
+//example 1
+function showMessage(from, text) { // arguments: from, text
+  alert(from + ': ' + text);
+}
+showMessage('Ann', 'Hello!'); // Ann: Hello! (*)
+showMessage('Ann', "What's up?"); // Ann: What's up? (**)
+
+//example 2
+function showMessage(from, text) {
+//change the variable inside the function
+  from = '*' + from + '*'; // make "from" look nicer
+  alert( from + ': ' + text );
+}
+
+let from = "Ann";
+showMessage(from, "Hello"); // *Ann*: Hello // the function variable now has a new value 
+
+// the value of "from" is the same, the function modified a local copy
+alert( from ); // Ann
+
