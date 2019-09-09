bb00534a6d73aca53b810592b7270fe8095783f7
ricardoaguiar <jrb45dk@gmail.com>
2019-09-09 17:39:01 +0200

    JS1 W2 homework

--- javascript/javascript1/week2/homework/what-to-wear.js
+++ javascript/javascript1/week2/homework/what-to-wear.js
@@ -1,14 +1,19 @@
-console.log("what to wear");
+console.log("what to wear exercise");
 
+function whatToWear(temp) { //passing a temp parameter for evaluation inside the function
 
-function whatToWear(g) {
+    if (temp > 18 || temp == 18) { //if statement temp will be equal or higher than 18 we will use summer clothes. 
+        //I tried => 18 but the function fails and returns the summer statment even when we pass 17. 
+        //It works with or || statment. 
 
-    let temp = 18;
-if (temp > 18)
-console.log("")
-
+        console.log(`The temperature today is likely to stay above ${temp} so you can go out in shorts and a t-shirt.`); //using template string / template literals - dymamic variables inside a string. This way we don't need to concatenate the temp with the string. 
+    } else if (temp < 18) { //if it is below 18, better take a jacket
+        console.log("The temperature today is below " + temp + " so jacket would be a good idea."); //using double quotes we need to concatenate the string with the value being passed into the parameter. 
+    }
 }
 
-const clothesToWear = whatToWear(18);
-
+let clothesToWear = whatToWear(18);
 console.log(clothesToWear); // Logs out: "shorts and a t-shirt"
+
+whatToWear(19); //The temperature today is likely to stay above 19 so you can go out in shorts and a t-shirt 
+whatToWear(10); //The temperature today is below 10 so jacket would be a good idea
