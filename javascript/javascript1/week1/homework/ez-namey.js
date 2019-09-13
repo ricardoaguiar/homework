59f775c02c477c2a24c4e957c88e223092d5ae3a
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-09-13 15:37:56 +0200

    Update ez-namey.js

--- javascript/javascript1/week1/homework/ez-namey.js
+++ javascript/javascript1/week1/homework/ez-namey.js
@@ -2,10 +2,12 @@ console.log("Ez Namey Starup");
 
 const firstWords = ["Super", "Corporate", "Easy", "Never", "Awesome", "Big", "Done", "It", "Your", "Best"];
 const secondWords = ["Fast", "Winner", "Give", "Finger", "GotIt", "Been", "Tomorrow", "Never", "Dies", "Gold"];
-const randomNumber = Math.floor(Math.random() * 10) + 0;
+const randomNumber = Math.floor(Math.random() * 10) + 0; //get a random number for the first array
+const randomNumber2 = Math.floor(Math.random() * 10) + 0; //get a random number for the secon array
+
 
 const startupName1 = firstWords[randomNumber]; //randomize the first array
-const startupName2 = secondWords[randomNumber]; //randomize the second array
+const startupName2 = secondWords[randomNumber2]; //randomize the second array
 
 const startupName = `${startupName1}  ${startupName2}`;
 // const b = Array.from(firstWords, x => x % 2 == 0);
