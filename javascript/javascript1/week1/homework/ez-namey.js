7ecdde9799fb6327f9f944f2d5f2c1bb7a4d03a3
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-09-16 21:04:15 +0200

    updated with a function

--- javascript/javascript1/week1/homework/ez-namey.js
+++ javascript/javascript1/week1/homework/ez-namey.js
@@ -2,20 +2,41 @@ console.log("Ez Namey Starup");
 
 const firstWords = ["Super", "Corporate", "Easy", "Never", "Awesome", "Big", "Done", "It", "Your", "Best"];
 const secondWords = ["Fast", "Winner", "Give", "Finger", "GotIt", "Been", "Tomorrow", "Never", "Dies", "Gold"];
-const randomNumber = Math.floor(Math.random() * 10) + 0; //get a random number for the first array
-const randomNumber2 = Math.floor(Math.random() * 10) + 0; //get a random number for the secon array
+
+// const randomNumber = Math.floor(Math.random() * 10) + 0;
+// const randomNumber2 = Math.floor(Math.random() * 10) + 0;
+
+// const startupName1 = firstWords[randomNumber]; //randomize the first array
+// const startupName2 = secondWords[randomNumber2]; //randomize the second array
+
+// const startupName = `${startupName1} ${startupName2}`;
+// // const b = Array.from(firstWords, x => x % 2 == 0);
+
+// console.log(firstWords);
+// console.log(secondWords);
+// console.log(randomNumber);
+// console.log(randomNumber2);
+// console.log(startupName1);
+// console.log(startupName2);
+// console.log(startupName);
+
+console.log("function starts here");
+
+// function arrayCount(arr) {
+//     let randomItem = Math.floor(Math.random() * 10) + 0;
+//     for (let i = 0; i < arr.lenght; i++) {
+//         console.log((arr[i][randomItem]));
+//     }
+// }
+// console.log(firstWords);
+
+// firstWords.forEach((a, b) => console.log(a + ` ${secondWords[b]}`));
 
 
-const startupName1 = firstWords[randomNumber]; //randomize the first array
-const startupName2 = secondWords[randomNumber2]; //randomize the second array
+//based on https://timonweb.com/tutorials/how-to-get-a-random-value-from-a-javascript-array/
 
-const startupName = `${startupName1}  ${startupName2}`;
-// const b = Array.from(firstWords, x => x % 2 == 0);
-
-console.log(firstWords);
-console.log(secondWords);
-console.log(randomNumber);
-console.log(randomNumber2);
-console.log(startupName1);
-console.log(startupName2);
-console.log(startupName);
+function randomFirstName(firstWords, secondWords) {
+    //combine the math.random result from each array in one return statement
+    return firstWords[Math.floor(Math.random() * firstWords.length)] + " " + secondWords[Math.floor(Math.random() * secondWords.length)];
+}
+console.log(randomFirstName(firstWords, secondWords));
