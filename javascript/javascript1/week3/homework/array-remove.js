4b2eefddbdc4db5afd13c78da4cee56f892a1eb4
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-09-23 07:14:04 +0200

    Update array-remove.js

--- javascript/javascript1/week3/homework/array-remove.js
+++ javascript/javascript1/week3/homework/array-remove.js
@@ -1,1 +1,23 @@
+console.log('-----------------------------------');
+console.log('Remove array element');
 
+
+console.log('array before'); //log array before loop
+
+const names = ['Peter', 'Ahmad', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala'];
+console.log(names);
+
+const nameToRemove = 'Ahmad';
+
+// Write some code here
+for (i =0; i < names.length; i++) {
+  if (names[i] === nameToRemove) {
+    names.splice(i, 1);
+    // i--;
+  }
+}
+
+// Code done
+console.log('array after'); //log array after loop
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
