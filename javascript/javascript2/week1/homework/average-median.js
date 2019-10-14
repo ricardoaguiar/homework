2291a012630621919c5a2c7cc4f74cf03c3ab5cc
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-10-14 17:58:34 +0200

    Create average-median.js

---
+++ javascript/javascript2/week1/homework/average-median.js
@@ -0,0 +1,48 @@
+// use the functions on this array
+const housePrices = [3000000, 3500000, 1300000, 40000000, 100000000, 8000000, 2100000];
+
+console.log('----------------------');
+console.log('average');
+
+function arrayAverage(housePrices){
+    //Find the sum
+    let sum = 0;
+    for(let i in housePrices) {
+        sum += housePrices[i];
+    }
+    //Get the length of the array
+    let ct = housePrices.length;
+    //Return the average / mean.
+    return (sum / ct);
+}
+
+const averageResult = arrayAverage(housePrices);
+console.log(averageResult.toFixed(2));
+
+
+console.log('----------------------');
+console.log('median');
+
+function arrayMedian(housePrices) {
+    if(housePrices.length === 0) return 0;
+    housePrices.sort(function(a,b){
+        return a-b;
+    });
+    let mid = Math.floor(housePrices.length / 2);
+    if(housePrices.length % 2)
+    return housePrices[mid];
+    return (housePrices[mid - 1] + housePrices[mid]) / 2.0;
+}
+console.log(arrayMedian(housePrices));
+
+
+// function medianPrices(housePrices) {   
+// housePrices => {
+//     const mid = Math.floor(housePrices.lenght / 2 ),
+//     return housePrices.lenght % 2 !==0 ? prices[mid] : (nums[mid])    
+//    	prices = [...housePrices].sort((a, b) => a - b);
+// };
+// }
+
+// const medianResults = medianPrices(housePrices);
+// console.log(medianResults);
