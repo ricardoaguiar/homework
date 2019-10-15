856794ce9975f7d681e07c26eea791eaff59d9b2
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-10-15 06:26:13 +0200

    Update spirit-animal.js

--- javascript/javascript2/week1/homework/spirit-animal.js
+++ javascript/javascript2/week1/homework/spirit-animal.js
@@ -10,10 +10,8 @@ console.log('spirit animal');
 
 const animalList = ['Crying Wolf', 'Hungry Bear', 'Godzilla', 'Skinny Cat', 'Stinky Fish', 'Fat Gorilla', 'Neckless Girafe'];
 
-var rand = animalList[Math.floor(Math.random() * animalList.length)];
-
-
 const button = document.querySelector("#getName");
+const button2 = document.querySelector("#getNewName");
 console.log(button);
 
 // let nameValue = document.getElementById('nameInput').innerHTML;
@@ -21,19 +19,40 @@ console.log(button);
 // console.log(nameValue);
 
 
+var rand = animalList[Math.floor(Math.random() * animalList.length)];
 
 button.addEventListener("click", function () {
     console.log("button was clicked");
-    var rand = animalList[Math.floor(Math.random() * animalList.length)];
     const name = document.querySelector("#nameInput").value;
     if (name === "") {
         alert("Name must be filled out");
         // return false;
     } if (name !== "") {
     const p = document.createElement('p');
-    document.getElementById('name2').innerHTML = name;
-    // document.getElementById('name2') = name;
-    document.getElementById('animalName').innerHTML = rand;
+    document.getElementById('animalResult').innerHTML = `
+    <div>
+    <p>${name} - ${rand}</p>
+    </div>
+    `
+    // document.getElementById('name2').innerHTML = name;
+    // // document.getElementById('name2') = name;
+    // document.getElementById('animalName').innerHTML = rand;
     }
 // return;
+});
+
+button2.addEventListener("click", function () {
+    var rand = animalList[Math.floor(Math.random() * animalList.length)];
+    const name = document.querySelector("#nameInput").value;
+    if (name === "") {
+        alert("Name must be filled out");
+    } 
+    if (name !== "") {
+    const p = document.createElement('p');
+    document.getElementById('animalResult').innerHTML = `
+    <div>
+    <p>${name} - ${rand}</p>
+    </div>
+    `
+    }
 });
