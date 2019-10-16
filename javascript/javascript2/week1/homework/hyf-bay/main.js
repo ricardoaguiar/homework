667176d8399bb1648aff63161b21d7203ce07b9e
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-10-16 08:21:22 +0200

    Update main.js

--- javascript/javascript2/week1/homework/hyf-bay/main.js
+++ javascript/javascript2/week1/homework/hyf-bay/main.js
@@ -1,3 +1,16 @@
 console.log('Script loaded');
 
+const testProductNames = ['Flat screen', 'Mobile phone', 'Wallet'];
+renderProducts(testProductNames); // Should add 3 li's to the ul under the products section with Flat screen, Mobile phone, Wallet text
+
+const list = document.querySelector('.products');
+
+for(let i = 0; i < testProductNames.length; i++) {
+    const p = testProductNames[i];
+    const li = document.createElement('li');
+    li.innerText = product;
+    list.appendChild(li);
+}
+
+
 console.log(getAvailableProducts());
