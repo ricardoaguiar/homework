12d91b3d7244beb5a3de610f33f387c0d2126637
ricardoaguiar <jrb45dk@gmail.com>
2019-10-23 15:47:05 +0200

    update

---
+++ javascript/javascript2/week2/main.js
@@ -0,0 +1,69 @@
+/**
+ * Get random integer between two numbers, found here: https://stackoverflow.com/a/7228322
+ * @param {integer} min - The min number
+ * @param {integer} max - The max number
+ * @returns {Number} Random number between min and max
+ */
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+/**
+ * Get an array with listing objects with random color and speed
+ * @param {integer} numberOfListings - The number of listings 
+ * @returns {array} Array containing the listing objects
+ */
+function generateListings(numberOfListings) {
+    const listings = [];
+
+    const listingType = ['House', 'Apartment', 'Shed', 'Dorm', 'Farm'];
+    const listingFacilities = ['Parkering', 'Elevator', 'Altan', 'Have', 'Husdyr'];
+
+    for (let i = 0; i < numberOfListings; i++) {
+        const listing = {};
+        const randomTypeIndex = randomIntFromInterval(0, listingType.length - 1);
+        const numberOfFacilities = randomIntFromInterval(1, listingFacilities.length - 1);
+        const facilities = [];
+        for (let i = 0; i < numberOfFacilities; i++) {
+            const randomIndexFacilities = randomIntFromInterval(0, listingFacilities.length - 1);
+            const randomFacility = listingFacilities[randomIndexFacilities];
+
+            if (!(facilities.includes(randomFacility))) {
+                facilities.push(randomFacility);
+            }
+        }
+
+        listing.type = listingType[randomTypeIndex];
+        listing.facilities = facilities;
+        listing.price = randomIntFromInterval(1, 10000);
+        listing.hasGarden = Boolean(randomIntFromInterval(0, 1));
+        listing.size = randomIntFromInterval(12, 1000);
+
+        listings.push(listing);
+    }
+
+    return listings;
+}
+
+
+
+const result = generateListings(37);
+result.forEach((listing) => console.log(listing.size))
+// console.log(result);
+
+// const listPrice = generateListings(37);
+const arr = result.map((prices) => prices.price)
+console.log(arr);
+
+
+const cheapListings = generateListings(37);
+const obj = cheapListings.filter((cheap) => cheap.price <= 5000)
+console.log(obj);
+
+const expList = result.filter((value) => value.price >= 5000)
+.map((value) => value.price)
+console.log(expList);
+
+const hasPark = result.filter((park) => park.facilities.includes('Parkering'))
+console.log(hasPark);
