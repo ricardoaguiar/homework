ed91a4616253b1e34492838c33bd85383273db3c
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-10-26 16:59:52 +0200

    homework

--- javascript/javascript2/week1/homework/spirit-animal.js
+++ javascript/javascript2/week1/homework/spirit-animal.js
@@ -1,91 +1,108 @@
 console.log('----------------------');
 console.log('spirit animal');
 
-// const button = document.querySelector("#getName");
-// console.log(button);
+// // Function to change the content of t2
+// function modifyText(new_text) {
+//     var t2 = document.getElementById("t2");
+//     t2.firstChild.nodeValue = new_text;    
+//   }
+   
+//   // Add event listener to table with an arrow function
+//   var el = document.getElementById("outside");
+//   el.addEventListener("click", () => { modifyText("four"); }, false);
 
-// button.addEventListener("click", function() {
-//     alert('button was clicked');    
-// });
+
+
 
 const animalList = ['Crying Wolf', 'Hungry Bear', 'Godzilla', 'Skinny Cat', 'Stinky Fish', 'Fat Gorilla', 'Neckless Girafe'];
 
-const button = document.querySelector("#getName");
-const button2 = document.querySelector("#getNewName");
-// console.log(button);
 
-// let nameValue = document.getElementById('nameInput').innerHTML;
-// console.log(name);
-// console.log(nameValue);
+// var rand = animalList[Math.floor(Math.random() * animalList.length)];
+
+// const getNameBtn = document.querySelector('#getName');
+// const getNewNameBtn = document.querySelector('#getNewName');
+// const animalResult = document.getElementById('animalResult');
+// const selectedOpt = document.getElementById('selectedOption');
+// const nameInput = document.querySelector('name');
+// const nameOutput = document.getElementById('nameOutput');
+// const clickButton = document.getElementById('clickButton');
+const enterName = document.getElementById('enterName');
+const hoverInput = document.getElementById('hoverInput');
 
 
-var rand = animalList[Math.floor(Math.random() * animalList.length)];
+const input = document.querySelector('input'); //select input field
+const log = document.getElementById('values') //get id values and assing to log
 
-button.addEventListener("click", function () {
-    console.log("button was clicked");
-    const name = document.querySelector("#nameInput").value;
-    if (name === "") {
-        alert("Name must be filled out");
-        // return false;
-    } if (name !== "") {
-    const p = document.createElement('p');
-    document.getElementById('animalResult').innerHTML = `
-    <div>
-    <p>${name} - ${rand}</p>
-    </div>
-    `
-    // document.getElementById('name2').innerHTML = name;
-    // // document.getElementById('name2') = name;
-    // document.getElementById('animalName').innerHTML = rand;
-    }
-// return;
-});
-
-button2.addEventListener("click", function () {
-    var rand = animalList[Math.floor(Math.random() * animalList.length)];
-    const name = document.querySelector("#nameInput").value;
-    if (name === "") {
-        alert("Name must be filled out");
-    } 
-    if (name !== "") {
-    const p = document.createElement('p');
-    document.getElementById('animalResult').innerHTML = `
-    <div>
-    <p>${name} - ${rand}</p>
-    </div>
-    `
-    }
-});
-
-const select = document.querySelector('select');
-
-select.onchange = function() {
-    let selOption = spiritAnimal.list[spiritAnimal.list.selectedIndex].value;
-    let selectedOpt = document.getElementById("selectedOption")
-
-    console.log(selOption);
-    if (selOption === 'clickButton') {
-        // alert('click button')
-        selectedOpt.innerHTML = ('Your current option is to click a button.');
-    } 
-    if (selOption === 'enterName') {
-        // alert('enter name')
-        document.getElementById("getName").disabled = true;
-        document.getElementById("getNewName").disabled = true;
-        selectedOpt.innerHTML = ('Your current option is to type your name.');
-        document.getElementById("nameInput").setAttribute("mouseenter", `${name}`);
-        document.getElementById("animalResult").innerHTML = `${name} - ${rand}`;
-
-        
-    } 
-    if (selOption === 'hoverInput') {
-        // alert('hover input')
-        document.getElementById("getName").disabled = true;
-        document.getElementById("getNewName").disabled = true;
-        selectedOpt.innerHTML = ('Your current option is to mouse over the input field.');
-        hoverOption.innerHTML = ('Type your name on the name field to get your spirit animal name');
-        document.getElementById("nameInput").setAttribute("mouseover", `${name}`);
-        document.getElementById("animalResult").innerHTML = `${name} - ${rand}`;
-        
-    } 
+// input.addEventListener('input', updateValue); //add eventListener to input
+input.oninput = function() {
+  log.innerText = input.{getAnimalName()};
 }
+
+function updateValue(e) {
+  log.innerText = e.target.value;
+}
+
+
+function getAnimalName() {
+  return animalSpirit = animalList[Math.floor(Math.random() * animalList.length)];
+}
+
+
+// getNameBtn.addEventListener("click", function() {
+//     console.log("button was clicked");
+//     const name = document.getElementById("#nameInput").value;   
+//     if (name === "") {
+//         alert('Please select an option and fill out your name.');
+//      } else if (name !== "") {
+//          animalResult.innerText = `${name} - ${rand}`;
+//      }
+//     });
+
+// getNewNameBtn.addEventListener("click", function () {
+//     const rand = animalList[Math.floor(Math.random() * animalList.length)];
+//     // const name = document.querySelector("#nameInput").value;
+//     if (name === "") {
+//         animalResult.innerText = `Ops... we need a name first!`;
+//     } else {
+//     animalResult.innerText = `${name} - ${rand}`
+//     }
+// });
+
+// const select = document.querySelector('select');
+// getNameBtn.disabled = true;
+// getNewNameBtn.disabled = true;
+
+// select.onchange = function(selOption) {
+//     selOption = spiritAnimal.list[spiritAnimal.list.selectedIndex].value;
+//     // const name = document.querySelector("#nameInput").value;
+
+//     if(selOption === '') {
+//         const name = document.getElementById('name');
+//         name.addEventListener('mouseenter', e => {
+//               name.style.border = '5px dotted orange';
+//               enterEventCount++;
+//               animalResult.innerText = `Type your name on this field`;;
+//             });
+//     }
+
+//     if (selOption === 'enterName') {
+//         // alert('enter name')
+//         selectedOpt.innerText = ('Your current option is to type your name.');
+//         if ( name === "") {
+//             alert('please enter your name');
+//         } else {                    
+//             nameInput.addEventListener('mouseover', e => {
+//                  nameOutput.innerText = nameInput.value;
+//         });
+//     } 
+//          if (selOption === 'hoverInput') {
+//         // alert('hover input')
+//         getNameBtn.disabled = true;
+//         getNewNameBtn.disabled = true;
+//         selectedOpt.innerText = ('Your current option is to mouse over the input field.');
+//         hoverOption.innerText = ('Type your name on the name field to get your spirit animal name');
+//         document.getElementById("nameInput");
+
+//     } 
+// } 
+// }
