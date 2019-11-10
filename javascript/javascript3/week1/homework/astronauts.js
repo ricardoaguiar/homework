cf57699f90ec0f100b193ec8f6aea60440b20884
ricardoaguiar <jrb45dk@gmail.com>
2019-11-10 08:40:02 +0100

    js3 w1

--- javascript/javascript3/week1/homework/astronauts.js
+++ javascript/javascript3/week1/homework/astronauts.js
@@ -1,6 +1,25 @@
 let thePeople = [], theName, theCraft;
 dataObj = {};
 
+fetch ('http://api.open-notify.org/astros.json')
+.then (function (resp) {
+  return resp.json ();
+})
+.then (function (data) {
+  console.log (data);
+  theName = data.name;
+  theCraft = data.craft;
+  thePeople = data.people;
+  dataObj = data;
+  showData ();
+
+  dataObj.people.forEach(person => {
+    document.getElementById('names').innerText += person.name 
+  });
+
+});
+
+
 let showData = function () {
   for (let prop in dataObj) {
     console.log (prop);
@@ -8,15 +27,4 @@ let showData = function () {
   }
 };
 
-fetch ('http://api.open-notify.org/astros.json')
-  .then (function (resp) {
-    return resp.json ();
-  })
-  .then (function (data) {
-    console.log (data);
-    theName = data.name;
-    theCraft = data.craft;
-    thePeople = data.people;
-    dataObj = data;
-    showData ();
-  });
+
