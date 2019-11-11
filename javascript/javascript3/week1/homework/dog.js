e7aa82c612464dc63a3062cd79034f3e1311ba41
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-11-11 21:39:29 +0100

    Update dog.js

--- javascript/javascript3/week1/homework/dog.js
+++ javascript/javascript3/week1/homework/dog.js
@@ -11,9 +11,21 @@ fetch(url)
     .then(data => {
         console.log('data:', data);
         console.log(data.message);
-
+        let randomImg = data.message;
+    //     let li = document.createElement(`<img src="${randomImg}" />`);
+    //     ul.appendChild(li);
+    
     });
+    
+function showImg() {
+    const img = document.createElement('img');
+    const li = document.createElement('li');
+}
 
-    // let img = document.createElement('li');
-    // let li = document.createElement(`<img src='${message}' />`);
+function createNode(element) {
+    return document.createElement(element); // Create the type of element you pass in the parameters
+  }
 
+  function append(parent, el) {
+    return parent.appendChild(el); // Append the second parameter(element) to the first one
+  }
