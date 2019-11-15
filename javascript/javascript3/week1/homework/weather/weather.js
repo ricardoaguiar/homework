7737eacd001a5852bb0caf6a13e0c170b8d64729
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-11-15 07:12:54 +0100

    update weather.js

--- javascript/javascript3/week1/homework/weather/weather.js
+++ javascript/javascript3/week1/homework/weather/weather.js
@@ -1,15 +1,7 @@
 // API key daf90bf3cb90d58f344174ca345725da;
 //unsplash auth 5e58f855051d6436bc43a9b1a127727f330c137360afcdcd32c1252f27955d79
 
-//function to create elements
-function createNode (element) {
-  return document.createElement (element);
-}
 
-//function to append elements to the DOM
-function append (parent, el) {
-  return parent.appendChild (el);
-}
 
 //declare elements
 let ul = createNode ('ul');
@@ -19,10 +11,9 @@ let input = createNode ('input');
 let button = createNode ('button');
 let p = createNode ('p');
 let h1 = createNode ('h1');
-//append elements
-// document.body.appendChild(ul);
-// append(ul, li);
-// append(li, img);
+
+
+// append(h1);
 document.body.appendChild (h1);
 h1.innerText = 'Weather in your city';
 h1.marginBottom = '1em';
@@ -52,7 +43,7 @@ button.style.borderStyle = 'solid';
 button.style.borderWidth = 'thin';
 button.style.marginLeft = '10px';
 button.style.fontSize = '.8em';
-button.id = 'cityWeather';
+button.id = 'getWeather';
 
 p.style.fontFamily = 'Arial';
 p.style.fontSize = '0.8em';
@@ -60,33 +51,52 @@ p.style.textTransform = 'uppercase';
 p.style.fontWeight = '800';
 
 //get new weather by clicking the button
-document.getElementById ('cityWeather').onclick = function () {
- 
+// const city = document.getElementById('#cityName');
+// const getWeather = document.getElementById("")
+
+document.getElementById ('cityName').onclick = function () {
   if (input.value === '') {
-    document.body.appendChild(p).innerText = 'Please enter a city name';
-  } else {
-    document.body.appendChild(p).innerText = input.value + ' weather';
+    return document.body.appendChild(p).innerText = 'Please enter a city name'; 
   }
   console.log (input.value);
+}
+ 
+document.getElementById ('getWeather').onclick = function checkWeather() {
+  fetch (`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=daf90bf3cb90d58f344174ca345725da`)
 
-  fetch (
-    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=daf90bf3cb90d58f344174ca345725da`
-  )
-    .then (function (resp) {
+  .then (function (resp) {
       return resp.json ();
     })
-    // .then (function(data) {
-    //    for (const i in data) {
-    //     if (data.hasOwnProperty(i)) {
-    //        console.log(`${i}: ${data[i]}`);
-    //    }
-    //   }
+    /* hasOwnProperty method
     .then (function(data) {
-      Object.entries(data).forEach (([key, value]) => {
+       for (const i in data) {
+        if (data.hasOwnProperty(i)) {
+           console.log(`${i}: ${data[i]}`);
+       }
+      }
+*/
+    //forEach method
+    .then (function(data) {
+      Object.entries(data).forEach(([key, value]) => {
         console.log (`${key}: ${value}`);
-
-        // document.body.appendChild(p).innerText = `${key}: ${value}`;
-
+        document.body.appendChild(p).innerText = input.value + ' weather';
+        append(ul, li).innerText = `${key}: ${value}`;
       });
     });
-};
+  };
+
+
+
+//function to create elements
+function createNode (element) {
+  return document.createElement (element);
+}
+
+//function to append elements to the DOM
+function append (parent, el) {
+  return parent.appendChild (el);
+}
+
+//append elements
+document.body.appendChild(ul);
+ul.style.listStyle = 'none';
