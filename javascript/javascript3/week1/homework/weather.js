02157fadd759c3710bc9c05b656adbb4bfa7979d
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-11-15 17:36:31 +0100

    Update weather.js

--- javascript/javascript3/week1/homework/weather.js
+++ javascript/javascript3/week1/homework/weather.js
@@ -19,10 +19,9 @@ let input = createNode ('input');
 let button = createNode ('button');
 let p = createNode ('p');
 let h1 = createNode ('h1');
+
+
 //append elements
-// document.body.appendChild(ul);
-// append(ul, li);
-// append(li, img);
 document.body.appendChild (h1);
 h1.innerText = 'Weater in your city';
 h1.marginBottom = '1em';
@@ -42,6 +41,7 @@ input.style.paddingLeft = '1em';
 input.style.marginBottom = '1em';
 input.style.fontSize = '.8em';
 input.id = 'cityName';
+input.setAttribute("class", "weatherApp");
 
 //append and style search button
 document.body.appendChild (button).innerText = 'Search';
@@ -53,40 +53,49 @@ button.style.borderWidth = 'thin';
 button.style.marginLeft = '10px';
 button.style.fontSize = '.8em';
 button.id = 'cityWeather';
+button.setAttribute("class", "weatherApp");
 
 p.style.fontFamily = 'Arial';
 p.style.fontSize = '0.8em';
 p.style.textTransform = 'uppercase';
 p.style.fontWeight = '800';
 
+li.style.fontFamily = 'Arial';
+li.style.fontSize = '0.8em';
+li.style.fontWeight = '400';
+
+
 //get new weather by clicking the button
-document.getElementById ('cityWeather').onclick = function () {
- 
+
+document.getElementById ('cityWeather').onclick = function() { 
   if (input.value === '') {
     document.body.appendChild(p).innerText = 'Please enter a city name';
   } else {
     document.body.appendChild(p).innerText = input.value + ' weather';
   }
   console.log (input.value);
-
-  fetch (
-    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=daf90bf3cb90d58f344174ca345725da`
-  )
-    .then (function (resp) {
-      return resp.json ();
-    })
+  
+  document.body.appendChild(ul);
+  
+  fetch (`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=daf90bf3cb90d58f344174ca345725da`)
+    .then (res => res.json())
     // .then (function(data) {
     //    for (const i in data) {
     //     if (data.hasOwnProperty(i)) {
     //        console.log(`${i}: ${data[i]}`);
     //    }
     //   }
-    .then (function(data) {
-      Object.entries(data).forEach (([key, value]) => {
-        console.log (`${key}: ${value}`);
-
-        // document.body.appendChild(p).innerText = `${key}: ${value}`;
-
+    .then (data => {
+      console.log(data);
+      // Object.entries(data).map(([key, value]) => {
+      //   console.log (`${key}: ${value}`);        
+      //     document.body.appendChild(p).innerText =  data.main.visibility;
+      
+      Object.getOwnPropertyNames(data).forEach((val, idx, array) => {
+        console.log(val + ' -> ' + data[val]); 
+        append(ul, li).innerText = `Current temperature in ${input.value}: ` + data.main.temp;
+        append(li, img).innerText = data.weather[0].icon;
+      
       });
-    });
-};
+      });
+    }
