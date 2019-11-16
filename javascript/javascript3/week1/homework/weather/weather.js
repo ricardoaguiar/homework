94f94c45bb934b214d5909216b51f21583dbe09c
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-11-16 12:31:10 +0100

    Update weather.js

--- javascript/javascript3/week1/homework/weather/weather.js
+++ javascript/javascript3/week1/homework/weather/weather.js
@@ -105,9 +105,9 @@ ul.style.flexFlow = 'column';
       navigator.geolocation.getCurrentPosition(position => {
         long = position.coords.longitude;
         lat = position.coords.latitude;
-        const proxy = 'https://cors-anywhere.herokuapp.com/'; //avoid cors issue on localhost
-        const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=daf90bf3cb90d58f344174ca345725da`;
-        // const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&id=daf90bf3cb90d58f344174ca345725da`;
+        // const proxy = 'https://cors-anywhere.herokuapp.com/'; //avoid cors issue on localhost
+        // const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=daf90bf3cb90d58f344174ca345725da`;
+        const api = `http://cors.io/?https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&id=daf90bf3cb90d58f344174ca345725da`;
         fetch(api)
           .then(r => {
             console.log(r);
@@ -148,7 +148,7 @@ document.getElementById ('cityWeather').onclick = function () {
   console.log (input.value);
 
   fetch (
-    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=daf90bf3cb90d58f344174ca345725da&containerid:'openweathermap-widget-12'`
+    `http://cors.io/?https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=daf90bf3cb90d58f344174ca345725da&containerid:'openweathermap-widget-12'`
   )
     .then (res => res.json ())
     .then (data => {
