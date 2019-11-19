e96c0e41a42b4f28f3e03784bd9864b505763d35
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-11-19 21:04:04 +0100

    Update main.js

--- javascript/javascript3/week1/homework/giphy/main.js
+++ javascript/javascript3/week1/homework/giphy/main.js
@@ -4,10 +4,6 @@ function createNode (element) {
   return document.createElement (element);
 }
 
-function appendBody (e) {
-  return document.body.appendChild (e);
-}
-
 function append (parent, el) {
   return parent.appendChild (el);
 }
@@ -16,6 +12,9 @@ function getElId (id) {
   return document.getElementById (id);
 }
 
+function appendBody (e) {
+    return document.body.appendChild (e);
+  }
 
 let h1 = createNode ('h1');
 let input = createNode ('input');
@@ -24,40 +23,40 @@ let button = createNode ('button');
 let p = createNode ('p');
 let ul = createNode('ul');
 let li = createNode('li');
+let img = createNode('img');
 
 getElId (giphyApp);
 append (giphyApp, h1);
-append(giphyApp, input);
-append(giphyApp, button);
-append(giphyApp, inputNumber);
-append(giphyApp, ul);
-// append(ul,li);
+append (giphyApp, input);
+append (giphyApp, button);
+append (giphyApp, inputNumber);
+
 
 giphyApp.style.backgroundColor = '#ccc';
 h1.innerText = 'GIPHY APP';
 button.innerText = 'GIFS SEARCH'
-button.id = 'gifsearch';
+button.id = 'gifSearch';
 inputNumber.placeholder = 'number of gifs to display';
+// img.setAttribute('class', 'loadGifs');
 
 
-
-// let liIndex = 0;
-// li.setAttribute('id', 'li'+liIndex++);
-
-// const api = `https://api.openweathermap.org/data/2.5/weather?q=vejle&appid=daf90bf3cb90d58f344174ca345725da`;
-const api = `http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=tsVdxtFKdIvj8uPoxKnO2lgCkzId4h8y&limit=5'`;
-
-// document.getElementById ('giphyApp').onclick = () => {
-window.onload = () => {
-  fetch (api)
-    .then (r => {
-      console.log (r);
-      return r.json ();
-    })
-    .then (data => {
-      console.log ('success got data', data);
-      for (let prop in data) {
-          console.log(data[prop]);
-        }
-    });
+document.getElementById ('gifSearch').onclick = () => {
+  let imgCount = inputNumber.value;
+  if (imgCount > 0) {
+    // window.onload = () => {
+    fetch (
+        // `http://api.giphy.com/v1/gifs/search?q=dog&api_key=tsVdxtFKdIvj8uPoxKnO2lgCkzId4h8y&limit=3&offset=0&rating=G&lang=en'`
+        `http://api.giphy.com/v1/gifs/search?q=${input.value}&api_key=tsVdxtFKdIvj8uPoxKnO2lgCkzId4h8y&limit=${inputNumber.value}&offset=0&rating=G&lang=en'`
+        )
+      .then (r => {
+        return r.json ();
+      })
+      .then (data => {
+        const {data: [{images: {original: {url}}}]} = data;
+        appendBody(img).src = url + '.png';
+        p.innerText = "";
+      });
+  } else {
+    append (giphyApp, p).innerText = 'Please enter desire number of Giphys';
+  }
 };
