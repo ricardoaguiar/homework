6d843fa2b5a2e16e6d8b1107324ba43fb04dee11
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-11-27 07:12:20 +0100

    Update currency.js

--- javascript/javascript3/week3/shopping-cart/currency.js
+++ javascript/javascript3/week3/shopping-cart/currency.js
@@ -9,8 +9,9 @@ function getRates () {
     //iterates over the rates and paint to the DOM
     for (const [key, value] of Object.entries (rts)) {
       console.log (`key: ${key}, value: ${value}`);
-
+      //create an option for each currency
       let option = document.createElement('option');
+      //set a value for each currency
       option.setAttribute('value', `${key}`);
       option.innerText = `${key}: ${value}`;
       select.appendChild (option);
