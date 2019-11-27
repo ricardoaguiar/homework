de4510a4e051b9b1c4eae88fa775e58c0b5583b9
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-11-27 07:10:03 +0100

    Update currency.js

--- javascript/javascript3/week3/shopping-cart/currency.js
+++ javascript/javascript3/week3/shopping-cart/currency.js
@@ -6,7 +6,6 @@ function getRates () {
   .then (data => {
     const rts = data.rates; //assign the rates key to a variable
     // console.log (rts);
-    // console.log (typeof rts);
     //iterates over the rates and paint to the DOM
     for (const [key, value] of Object.entries (rts)) {
       console.log (`key: ${key}, value: ${value}`);
