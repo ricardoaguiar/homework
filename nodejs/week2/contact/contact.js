17f592577b8b02e05d3d616b1bfc9d9ecbcaa5d6
ricardoaguiar <jrb45dk@gmail.com>
2019-12-18 16:51:12 +0100

    class

---
+++ nodejs/week2/contact/contact.js
@@ -0,0 +1,12 @@
+const contactMe = contactContent => `
+    <h1>Contact</h1>
+        <p>This is a contact page</p>
+        <form>
+            <label>Contact me</label>
+            <input placeholder="Ricardo Aguiar">
+        <button type="button" class="btn btn-primary">Contact me</button>
+
+        </form>
+`;
+
+module.exports = contactMe;
---
+++ nodejs/week2/education/education.js
@@ -0,0 +1,9 @@
+const education = educationContent => {`
+    <ul>
+    <li>Communication & Marketing - UAM - Brazil</li>
+    <li>Photography - The Art Institute of Ft. Lauderdale - USA</li>
+    <li>Dansk Undervising - Dansk 2 & FVU trin 1 - 4 - Denmark</li>
+    </ul>
+`}
+
+module.export = education;
