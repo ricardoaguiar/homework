87ba7dbcc043cb363204245a1de84426d2726867
ricardo aguiar <barrios.dev@gmail.com>
2019-12-29 14:03:54 +0100

    sc update

--- javascript/javascript3/week3/shopping-cart/main.js
+++ javascript/javascript3/week3/shopping-cart/main.js
@@ -13,7 +13,8 @@ const productUL = document.querySelector("section.products ul");
 const productLI = document.createElement("li");
 const p = document.createElement("p");
 
-const cart = [];
+let cart = [];
+let buttonsDOM = [];
 
 const convertedCur = document.getElementById("converted-currency");
 const conversionResult = document.createElement("p");
@@ -56,12 +57,12 @@ class ShoppingCart {
   searchProduct() {}
 
   getTotal() {
-    const total = products.map(product => product.price);
-    let sum = 0;
-    for (let i = 0; i < total.length; i++) {
-      sum += total[i];
-    }
-    document.getElementById("total").innerHTML = sum;
+    // const total = products.map(product => product.price);
+    // let sum = 0;
+    // for (let i = 0; i < total.length; i++) {
+    //   sum += total[i];
+    // }
+    // document.getElementById("total").innerHTML = sum;
   }
   renderProducts(products) {
     console.log(products);
@@ -69,10 +70,9 @@ class ShoppingCart {
     products.forEach(product => {
       result += `
       <article class="product">
-                <figure class="img-container"><img src=${product.imgUrl} alt="product" class="product-img">
-                    <figcaption class="bag-btn" data-id=${product.id}><i class="fas fa-shopping-cart"></i>add to cart
-                    </figcaption>
-                </figure>
+                <div class="img-container"><img src=${product.imgUrl} alt="product" class="product-img">
+                </div>
+                <button class="bag-btn" data-id=${product.id}><i class="fas fa-shopping-cart"></i>add to cart</button>
                 <h3>${product.name}</h3>
                 <h4>$${product.price}</h4>
             </article>    
@@ -80,7 +80,44 @@ class ShoppingCart {
     });
     productDOM.innerHTML = result;
   }
+  getCartButton() {
+    const cartButtons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = cartButtons;
+    cartButtons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if (inCart) {
+        button.innerText = "In Cart";
+        button.disable = true;
+      }
+      button.addEventListener("click", event => {
+        event.target.innerText = "In Cart";
+        event.target.disable = true;
+        //get products
+        let cartItem = { ...Storage.getProduct(id), amount: 1 };
+        //add product to the cart
+        cart = [...cart, cartItem];
+        //save cart in local storage
+        Storage.saveCart(cart);
+        //set cart values
+        this.setCartValues(cart);
+        //display cart items
+        //show the cart
+      });
+    });
+  }
 
+  setCartValues(cart) {
+    let tmpTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tmpTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tmpTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+    console.log(cartTotal, cartItems);
+  }
   convertCurrency() {
     this.sel = sel;
     sel.addEventListener("change", function() {
@@ -102,7 +139,18 @@ class ShoppingCart {
   }
 }
 
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find(product => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const mac = new Products("mac", 3000);
@@ -114,7 +162,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   products
     .getProducts()
-    .then(products => shoppingCart.renderProducts(products));
+    .then(products => {
+      shoppingCart.renderProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      shoppingCart.getCartButton();
+    });
 
   // shoppingCart.convertCurrency();
   // shoppingCart.searchProduct();
